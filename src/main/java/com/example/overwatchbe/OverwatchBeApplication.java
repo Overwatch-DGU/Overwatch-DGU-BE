56838e3022aeeb9277f4f2ce20009a34cc61250b
sunny <122611609+sunnyanna0@users.noreply.github.com>
2024-11-19 17:19:11 +0900

    🎉tada: 초기 설정

---
+++ src/main/java/com/example/overwatchbe/OverwatchBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.overwatchbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverwatchBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverwatchBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/overwatchbe/OverwatchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.overwatchbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverwatchBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
