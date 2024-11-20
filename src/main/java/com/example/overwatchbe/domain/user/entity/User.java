c9a9b7e46b18864720ec577a565e554a25e28d73
sunny <122611609+sunnyanna0@users.noreply.github.com>
2024-11-21 04:13:39 +0900

    ✨:sparkles:로그인/로그아웃

---
+++ src/main/java/com/example/overwatchbe/domain/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.overwatchbe.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String level = "Bronze";
+
+    @Column(nullable = false)
+    private int coin = 0;
+}
