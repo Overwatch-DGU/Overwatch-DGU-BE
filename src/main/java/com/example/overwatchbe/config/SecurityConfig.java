05f46748a016efe00f21267fe45aa12bbfdc27d4
sunny <122611609+sunnyanna0@users.noreply.github.com>
2024-12-01 02:32:27 +0900

    ♻️:추가 수정

--- src/main/java/com/example/overwatchbe/config/SecurityConfig.java
+++ src/main/java/com/example/overwatchbe/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@ControllerAdvice
 @Configuration
 public class SecurityConfig {
 
--- src/main/java/com/example/overwatchbe/domain/user/dto/UserLoginRequest.java
+++ src/main/java/com/example/overwatchbe/domain/user/dto/UserLoginRequest.java
@@ -2,10 +2,12 @@ package com.example.overwatchbe.domain.user.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
 @Setter
 public class UserLoginRequest {
+    @NotNull
     private String email;
     private String password;
 }
